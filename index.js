6e36982719bfa578af9b4c1e334acb5f68bf0e85
Clark Van Oyen <clark@countable.ca>
2018-06-19 15:22:30 -0700

    whitespace

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ session = require('express-session');
  */
 
 defaults = {
-  port: 3000,
-  listen: true,
-  views: []
+    port: 3000,
+    listen: true,
+    views: []
 };
 
 
@@ -33,112 +33,112 @@ defaults = {
  */
 
 set_defaults = function(app) {
-  var k, results, v, view_dirs;
-  if (!app.get("app_name")) {
-    app.set("app_name", process.cwd().split("/").pop());
-  }
-  if (!app.get('dir')) {
-    app.set('dir', process.cwd());
-  }
-  view_dirs = defaults.views.push(app.get('views'));
-  results = [];
-  for (k in defaults) {
-    v = defaults[k];
-    if (!app.get(k)) {
-      results.push(app.set(k, defaults[k]));
-    } else {
-      results.push(void 0);
+    var k, results, v, view_dirs;
+    if (!app.get("app_name")) {
+        app.set("app_name", process.cwd().split("/").pop());
     }
-  }
-  return results;
+    if (!app.get('dir')) {
+        app.set('dir', process.cwd());
+    }
+    view_dirs = defaults.views.push(app.get('views'));
+    results = [];
+    for (k in defaults) {
+        v = defaults[k];
+        if (!app.get(k)) {
+            results.push(app.set(k, defaults[k]));
+        } else {
+            results.push(void 0);
+        }
+    }
+    return results;
 };
 
 implied = module.exports = function(app, options) {
-  if (app == null) {
-    app = express();
-  }
-  set_defaults(app);
-  app.set('implied', implied);
-
-  /*
-   * apply the config.
-   */
-  var config_filename = process.env.CONFIG_FILENAME || 'config';
-  console.log('cff', config_filename);
-  (require(path.join(app.get('dir'), config_filename)))(app);
-  
-  // raven request handler must be first to capture stuff properly.
-  if (app.get('sentry_url')) {
-    var raven = require('raven');
-    app.set('raven', raven);
-    raven.config(app.get('sentry_url')).install();
-    app.use(raven.requestHandler());
-  }
-
-  app.set('server', http.Server(app));
-
-  process.nextTick(function() {
-    if (app.get('port') && options.listen) {
-      return (app.get('server')).listen(app.get("port"), function() {
-        console.log("Express server listening on port " + app.get("port"));
-        return console.log("Server running in the " + (app.get("env")) + " environment");
-      });
+    if (app == null) {
+        app = express();
     }
-  });
-  
-  app.plugin = function(plugin, opts) {
-    var child, i, len, plugin_instance, plugin_name, registered_plugins;
-    if (opts == null) {
-      opts = {};
-    }
-    plugin_instance = void 0;
-    plugin_name = opts.plugin_name;
+    set_defaults(app);
+    app.set('implied', implied);
 
     /*
-     * Initialize the plugin
-     * ---------------------
-     * Array of plugins
+     * apply the config.
      */
-    if (plugin instanceof Array) {
-      for (i = 0, len = plugin.length; i < len; i++) {
-        child = plugin[i];
-        this.plugin(child, opts);
-      }
-    } else if (typeof plugin === 'string') {
-      if (!implied[plugin]) {
-        throw "Plugin `" + plugin + "` was not found.";
-      }
-      app.plugin(implied[plugin], implied.util.extend({
-        plugin_name: plugin
-      }, opts));
+    var config_filename = process.env.CONFIG_FILENAME || 'config';
+    console.log('cff', config_filename);
+    (require(path.join(app.get('dir'), config_filename)))(app);
 
-      /*
-       * A Plugin class instance
-       */
-    } else if (plugin instanceof implied.util.Plugin) {
-      plugin_instance = new plugin(app);
-
-      /*
-       * A function, simlpy call it with the app, and it can do what it pleases.
-       */
-    } else if (typeof plugin === 'function') {
-      plugin(app, opts);
-      plugin_instance = plugin;
-    } else {
-      throw "Usage: app.plugin( plugin ) where plugin is of type <String> | <Function> | <Array of plugins> , but you used app.plugin(" + typeof plugin + ")";
+    // raven request handler must be first to capture stuff properly.
+    if (app.get('sentry_url')) {
+        var raven = require('raven');
+        app.set('raven', raven);
+        raven.config(app.get('sentry_url')).install();
+        app.use(raven.requestHandler());
     }
 
-    /*
-     * Register the plugin
-     * -------------------
-     */
-    if (plugin_instance && plugin_name) {
-      registered_plugins = app.get('plugins') || {};
-      registered_plugins[plugin_name] = plugin_instance;
-      return app.set('plugins', registered_plugins);
-    }
-  };
-  return app;
+    app.set('server', http.Server(app));
+
+    process.nextTick(function() {
+        if (app.get('port') && options.listen) {
+            return (app.get('server')).listen(app.get("port"), function() {
+                console.log("Express server listening on port " + app.get("port"));
+                return console.log("Server running in the " + (app.get("env")) + " environment");
+            });
+        }
+    });
+
+    app.plugin = function(plugin, opts) {
+        var child, i, len, plugin_instance, plugin_name, registered_plugins;
+        if (opts == null) {
+            opts = {};
+        }
+        plugin_instance = void 0;
+        plugin_name = opts.plugin_name;
+
+        /*
+         * Initialize the plugin
+         * ---------------------
+         * Array of plugins
+         */
+        if (plugin instanceof Array) {
+            for (i = 0, len = plugin.length; i < len; i++) {
+                child = plugin[i];
+                this.plugin(child, opts);
+            }
+        } else if (typeof plugin === 'string') {
+            if (!implied[plugin]) {
+                throw "Plugin `" + plugin + "` was not found.";
+            }
+            app.plugin(implied[plugin], implied.util.extend({
+                plugin_name: plugin
+            }, opts));
+
+            /*
+             * A Plugin class instance
+             */
+        } else if (plugin instanceof implied.util.Plugin) {
+            plugin_instance = new plugin(app);
+
+            /*
+             * A function, simlpy call it with the app, and it can do what it pleases.
+             */
+        } else if (typeof plugin === 'function') {
+            plugin(app, opts);
+            plugin_instance = plugin;
+        } else {
+            throw "Usage: app.plugin( plugin ) where plugin is of type <String> | <Function> | <Array of plugins> , but you used app.plugin(" + typeof plugin + ")";
+        }
+
+        /*
+         * Register the plugin
+         * -------------------
+         */
+        if (plugin_instance && plugin_name) {
+            registered_plugins = app.get('plugins') || {};
+            registered_plugins[plugin_name] = plugin_instance;
+            return app.set('plugins', registered_plugins);
+        }
+    };
+    return app;
 };
 
 implied.util = require('./util');
--- util.js
+++ util.js
@@ -1,4 +1,3 @@
-
 /*
  * Some utilities
  */
@@ -9,115 +8,115 @@ exec = require('child_process').exec;
 mongojs = require('mongojs');
 
 Plugin = (function() {
-  function Plugin() {
-    this.app = app;
-  }
+    function Plugin() {
+        this.app = app;
+    }
 
-  return Plugin;
+    return Plugin;
 
 })();
 
 me = module.exports = {
-  extend: function(obj) {
-    Array.prototype.slice.call(arguments, 1).forEach(function(source) {
-      var prop, results;
-      if (source) {
-        results = [];
-        for (prop in source) {
-          results.push(obj[prop] = source[prop]);
+    extend: function(obj) {
+        Array.prototype.slice.call(arguments, 1).forEach(function(source) {
+            var prop, results;
+            if (source) {
+                results = [];
+                for (prop in source) {
+                    results.push(obj[prop] = source[prop]);
+                }
+                return results;
+            }
+        });
+        return obj;
+    },
+    syscall: function(command, callback, throws) {
+        if (throws == null) {
+            throws = true;
         }
-        return results;
-      }
-    });
-    return obj;
-  },
-  syscall: function(command, callback, throws) {
-    if (throws == null) {
-      throws = true;
-    }
-    return exec(command, function(error, stdout, stderr) {
+        return exec(command, function(error, stdout, stderr) {
 
-      /*
-      #if stdout
-       *  console.log "stdout: " + stdout
-       */
-      if (stderr && throws) {
-        console.error(command);
-        throw "stderr: " + stderr;
-      }
-      if (error && throws) {
-        console.error(command);
-        throw "exec error: " + error;
-      }
-      return typeof callback === "function" ? callback(stdout, error || stderr) : void 0;
-    });
-  },
-  get_file_extension: function(fname) {
-    return fname.substr((Math.max(0, fname.lastIndexOf(".")) || 2e308) + 1);
-  },
-  flash: function(req, message_type, message) {
-    var base, m;
-    if (message_type && message) {
-      m = (base = req.session).messages != null ? base.messages : base.messages = {};
-      if (m[message_type] == null) {
-        m[message_type] = [];
-      }
-      return m[message_type].push(message);
-    }
-  },
-  Plugin: Plugin,
-  format: function(str, dict) {
-    return str.replace(/\{([^\}]+)\}/g, function(match, $1) {
-      return dict[$1] || '';
-    });
-  },
-  zpad: function(num, zeros) {
-    if (zeros == null) {
-      zeros = 0;
-    }
-    num = '' + num;
-    while (num.length < zeros) {
-      num = '0' + num;
-    }
-    return num;
-  },
-  oid_str: function(inp) {
-    return (me.oid(inp)).toString();
-  },
-  oid: function(inp) {
-    var byte, result;
-    if (inp === void 0) {
-      console.error('implied.util.oid: undefined objectid');
-      console.trace();
-      return inp;
-    }
-    if (inp instanceof mongojs.ObjectId) {
-      return inp;
-    } else if (inp.bytes) {
-      result = ((function() {
-        var i, len, ref, results;
-        ref = inp.bytes;
-        results = [];
-        for (i = 0, len = ref.length; i < len; i++) {
-          byte = ref[i];
-          results.push(implied.util.zpad(byte.toString(16), 2));
+            /*
+            #if stdout
+             *  console.log "stdout: " + stdout
+             */
+            if (stderr && throws) {
+                console.error(command);
+                throw "stderr: " + stderr;
+            }
+            if (error && throws) {
+                console.error(command);
+                throw "exec error: " + error;
+            }
+            return typeof callback === "function" ? callback(stdout, error || stderr) : void 0;
+        });
+    },
+    get_file_extension: function(fname) {
+        return fname.substr((Math.max(0, fname.lastIndexOf(".")) || 2e308) + 1);
+    },
+    flash: function(req, message_type, message) {
+        var base, m;
+        if (message_type && message) {
+            m = (base = req.session).messages != null ? base.messages : base.messages = {};
+            if (m[message_type] == null) {
+                m[message_type] = [];
+            }
+            return m[message_type].push(message);
         }
-        return results;
-      })()).join('');
-      return mongojs.ObjectId(result);
-    } else {
-      return mongojs.ObjectId('' + inp);
+    },
+    Plugin: Plugin,
+    format: function(str, dict) {
+        return str.replace(/\{([^\}]+)\}/g, function(match, $1) {
+            return dict[$1] || '';
+        });
+    },
+    zpad: function(num, zeros) {
+        if (zeros == null) {
+            zeros = 0;
+        }
+        num = '' + num;
+        while (num.length < zeros) {
+            num = '0' + num;
+        }
+        return num;
+    },
+    oid_str: function(inp) {
+        return (me.oid(inp)).toString();
+    },
+    oid: function(inp) {
+        var byte, result;
+        if (inp === void 0) {
+            console.error('implied.util.oid: undefined objectid');
+            console.trace();
+            return inp;
+        }
+        if (inp instanceof mongojs.ObjectId) {
+            return inp;
+        } else if (inp.bytes) {
+            result = ((function() {
+                var i, len, ref, results;
+                ref = inp.bytes;
+                results = [];
+                for (i = 0, len = ref.length; i < len; i++) {
+                    byte = ref[i];
+                    results.push(implied.util.zpad(byte.toString(16), 2));
+                }
+                return results;
+            })()).join('');
+            return mongojs.ObjectId(result);
+        } else {
+            return mongojs.ObjectId('' + inp);
+        }
+    },
+    unique: function(array) {
+        var a, i, len, val;
+        a = [];
+        for (i = 0, len = array.length; i < len; i++) {
+            val = array[i];
+            if (a.indexOf(val) === -1) {
+                a.push(val);
+            }
+        }
+        return a;
     }
-  },
-  unique: function(array) {
-    var a, i, len, val;
-    a = [];
-    for (i = 0, len = array.length; i < len; i++) {
-      val = array[i];
-      if (a.indexOf(val) === -1) {
-        a.push(val);
-      }
-    }
-    return a;
-  }
 };
